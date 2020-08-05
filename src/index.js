529cc17d284297066770b8b5ee0603a34e892832
flxbot <mail@felixboettcher.de>
2020-08-05 16:15:21 +0200

    edit package.json

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import Control from './Control'
-import Preview from './Preview'
+import Control from "./Control";
+import Preview from "./Preview";
 
-if (typeof window !== 'undefined') {
-  window.Control = Control
-  window.Preview = Preview
+if (typeof window !== "undefined") {
+  window.ColorControl = Control;
+  window.ColorPreview = Preview;
 }
 
-export { Control, Preview }
+export { Control as ColorControl, Preview as ColorPreview };
